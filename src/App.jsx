830fbaa0360c612139ea6c24d37e738f26be9b7f
Samratcodebase <samratcodebase@gmail.com>
2026-04-21 00:15:41 +0530

    Refactor App component: comment out Calculator and add state management hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Counter } from "./projects/counterProject/Counter";
 import Todo from "./projects/TodoProject/Todo";
 import { Meals } from "./projects/MealsProject/Meals";
 import { Calculator } from "./projects/Calculator/Calculator";
+import { useState } from "react";
 const App = () => {
+  const [input, setinput] = useState("");
+  const [value, setvalue] = useState("");
+
   return (
     <>
       {/* <Counter /> */}
       {/* <Meals /> */}
-      <Calculator />
+      {/* <Calculator /> */}
     </>
   );
 };
